7ec10eb5f5d65ee61faa77285f786788e4d52c9e
Benah0987 <88454033+Benah0987@users.noreply.github.com>
2023-03-23 06:52:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+//created afunction countdown with Parameter num
+function countDown(num) {
+  //in the while loop expression the num should be greater or equal to zero  
+  while (num >= 0) {
+    console.log(num);
+    num--;//decrement operator
+  }
+}
+countDown(10)//then we called the function
+
+//just for the fun of it
+function count(x) {
+    for (let i = x; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  count(10);
